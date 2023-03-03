3fdc22d9f3084034fdc69b042ed98b90573ca60f
mena <mena.rizkalla1@gmail.com>
2023-03-03 19:16:27 +0200

    add animation

--- app/src/main/java/com/example/dishapp/SplashActivity.kt
+++ app/src/main/java/com/example/dishapp/SplashActivity.kt
@@ -1,10 +1,15 @@
 package com.example.dishapp
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.WindowInsets
 import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import com.example.dishapp.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
@@ -24,5 +29,28 @@ class SplashActivity : AppCompatActivity() {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
             )
         }
+
+        val anim = AnimationUtils.loadAnimation(this,R.anim.anim_splash)
+        binding.appName.animation = anim
+
+        val splashAnimation = AnimationUtils.loadAnimation(this, R.anim.anim_splash)
+        binding.appName.animation = splashAnimation
+        splashAnimation.setAnimationListener(object : Animation.AnimationListener{
+            override fun onAnimationStart(p0: Animation?) {
+
+            }
+
+            override fun onAnimationEnd(p0: Animation?) {
+                Handler(Looper.getMainLooper()).postDelayed({
+                    startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                    finish()
+                },1000)
+            }
+
+            override fun onAnimationRepeat(p0: Animation?) {
+
+            }
+
+        })
     }
 }
