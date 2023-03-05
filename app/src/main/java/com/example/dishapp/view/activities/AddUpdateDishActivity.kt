8ab0659fc4226cbc2694fb09a4ae9f8c4fdad4b0
mena <mena.rizkalla1@gmail.com>
2023-03-05 02:50:48 +0200

    add dialog for image selection

--- app/src/main/java/com/example/dishapp/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/example/dishapp/view/activities/AddUpdateDishActivity.kt
@@ -1,12 +1,32 @@
 package com.example.dishapp.view.activities
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.dishapp.R
+import com.example.dishapp.databinding.ActivityAddUpdateDishBinding
+import com.example.dishapp.databinding.DialogCustomImageSelectionBinding
 
 class AddUpdateDishActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityAddUpdateDishBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_update_dish)
+        binding = ActivityAddUpdateDishBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.ivAddDishImage.setOnClickListener {
+            customImageSelectionDialog()
+        }
+
     }
+
+    private fun customImageSelectionDialog(){
+        val dialog = Dialog(this)
+        val binding : DialogCustomImageSelectionBinding =
+            DialogCustomImageSelectionBinding.inflate(layoutInflater)
+        dialog.setContentView(binding.root)
+
+            dialog.show()
+        }
 }
