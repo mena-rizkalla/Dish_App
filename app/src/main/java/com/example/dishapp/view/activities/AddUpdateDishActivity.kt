b8f70a0d518e9aff78d02c8a49a616b64ec5c20d
mena <mena.rizkalla1@gmail.com>
2023-03-06 02:14:22 +0200

    add permission using dexter

--- app/src/main/java/com/example/dishapp/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/example/dishapp/view/activities/AddUpdateDishActivity.kt
@@ -3,9 +3,19 @@ package com.example.dishapp.view.activities
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.dishapp.R
+import android.Manifest
+import android.Manifest.permission_group.CAMERA
+import android.content.Intent
+import android.provider.MediaStore
+import android.webkit.PermissionRequest
+import android.widget.Toast
 import com.example.dishapp.databinding.ActivityAddUpdateDishBinding
 import com.example.dishapp.databinding.DialogCustomImageSelectionBinding
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.DexterBuilder
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 
 class AddUpdateDishActivity : AppCompatActivity() {
     private lateinit var binding : ActivityAddUpdateDishBinding
@@ -21,12 +31,37 @@ class AddUpdateDishActivity : AppCompatActivity() {
 
     }
 
-    private fun customImageSelectionDialog(){
+    private fun customImageSelectionDialog() {
         val dialog = Dialog(this)
-        val binding : DialogCustomImageSelectionBinding =
+        val binding: DialogCustomImageSelectionBinding =
             DialogCustomImageSelectionBinding.inflate(layoutInflater)
         dialog.setContentView(binding.root)
 
-            dialog.show()
+        binding.tvCamera.setOnClickListener {
+
+            Dexter.withContext(this).withPermissions(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA
+            ).withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    report?.let {
+                        if (report.areAllPermissionsGranted()) {
+                            Toast.makeText(this@AddUpdateDishActivity,"you have permission now",Toast.LENGTH_SHORT).show()
+
+                        }
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    p0: MutableList<com.karumi.dexter.listener.PermissionRequest>?,
+                    p1: PermissionToken?
+                ) {
+                    //add alert dialog later
+                }
+
+            }).onSameThread().check()
+
         }
+    }
 }
