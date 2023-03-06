4e01ed5bc2b6e46a23205a760418923afdd4c211
mena <mena.rizkalla1@gmail.com>
2023-03-06 02:37:34 +0200

    request permission

--- app/src/main/java/com/example/dishapp/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/example/dishapp/view/activities/AddUpdateDishActivity.kt
@@ -5,8 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.Manifest
 import android.Manifest.permission_group.CAMERA
+import android.app.AlertDialog
+import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.net.Uri
 import android.provider.MediaStore
+import android.provider.Settings
 import android.webkit.PermissionRequest
 import android.widget.Toast
 import com.example.dishapp.databinding.ActivityAddUpdateDishBinding
@@ -38,10 +42,8 @@ class AddUpdateDishActivity : AppCompatActivity() {
         dialog.setContentView(binding.root)
 
         binding.tvCamera.setOnClickListener {
-
             Dexter.withContext(this).withPermissions(
                 Manifest.permission.READ_EXTERNAL_STORAGE,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE,
                 Manifest.permission.CAMERA
             ).withListener(object : MultiplePermissionsListener {
                 override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
@@ -57,11 +59,56 @@ class AddUpdateDishActivity : AppCompatActivity() {
                     p0: MutableList<com.karumi.dexter.listener.PermissionRequest>?,
                     p1: PermissionToken?
                 ) {
-                    //add alert dialog later
+                    showDialogForPermissions()
                 }
 
             }).onSameThread().check()
-
+            dialog.dismiss()
         }
+
+
+        binding.tvGallery.setOnClickListener {
+            Dexter.withContext(this).withPermissions(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA
+            ).withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    report?.let {
+                        if (report.areAllPermissionsGranted()) {
+                            Toast.makeText(this@AddUpdateDishActivity,"you have permission now",Toast.LENGTH_SHORT).show()
+
+                        }
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    p0: MutableList<com.karumi.dexter.listener.PermissionRequest>?,
+                    p1: PermissionToken?
+                ) {
+                    showDialogForPermissions()
+                }
+
+            }).onSameThread().check()
+            dialog.dismiss()
+        }
+
+        dialog.show()
+
+    }
+
+    private fun showDialogForPermissions(){
+        AlertDialog.Builder(this).setMessage("Some permissions are required first")
+            .setPositiveButton("Go to setting") { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName,null)
+                    intent.data = uri
+                    startActivity(intent)
+                }catch (e : ActivityNotFoundException){
+                    e.printStackTrace()
+                }
+            }.setNegativeButton("Cancel"){
+                    dialog, _ -> dialog.dismiss()
+            }.show()
     }
 }
