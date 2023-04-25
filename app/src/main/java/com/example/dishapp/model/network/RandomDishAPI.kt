97339c1cff33a44020dc403c58d17c2843ed31d1
mena <mena.rizkalla1@gmail.com>
2023-04-25 14:05:40 +0200

    Setting up the API URL

--- app/src/main/java/com/example/dishapp/model/network/RandomDishAPI.kt
+++ app/src/main/java/com/example/dishapp/model/network/RandomDishAPI.kt
@@ -3,15 +3,9 @@ package com.example.dishapp.model.network
 import com.example.dishapp.model.entities.RandomDish
 import com.example.dishapp.utils.Constants
 import io.reactivex.rxjava3.core.Single
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+
 import retrofit2.http.GET
 import retrofit2.http.Query
-
-
-private const val BASE_URL="https://api.spoonacular.com/"
-
-private val retrofit = Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build()
 
 interface RandomDishAPI {
 
---
+++ app/src/main/java/com/example/dishapp/model/network/RandomDishAPIService.kt
@@ -0,0 +1,25 @@
+package com.example.dishapp.model.network
+
+import com.example.dishapp.model.entities.RandomDish
+import com.example.dishapp.utils.Constants
+import io.reactivex.rxjava3.core.Single
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
+class RandomDishAPIService {
+
+    private val retrofit = Retrofit.Builder().baseUrl(Constants.BASE_URL).addConverterFactory(
+        GsonConverterFactory.create()).addCallAdapterFactory(RxJava3CallAdapterFactory.create()).build()
+
+    private val api = retrofit.create(RandomDishAPI::class.java)
+
+    fun getRandomDish() : Single<RandomDish.Recipes>{
+       return api.getRandomDishes(Constants.API_KEY,
+           Constants.LIMIT_LICENCE_VALUE,
+           Constants.TAGS_VALUE,
+           Constants.NUMBER_VALUE)
+    }
+
+}
