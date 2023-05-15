adfbc9699d413a88a1e76224f01e514f9e789c5e
mena <mena.rizkalla1@gmail.com>
2023-05-15 20:20:41 +0300

    remove unwanted imports

--- app/src/main/java/com/example/dishapp/viewmodel/RandomDishViewModel.kt
+++ app/src/main/java/com/example/dishapp/viewmodel/RandomDishViewModel.kt
@@ -1,22 +1,13 @@
 package com.example.dishapp.viewmodel
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.example.dishapp.model.entities.RandomDish
-import com.example.dishapp.model.network.RandomDishAPI
 import com.example.dishapp.model.network.RandomDishAPIService
-import com.example.dishapp.utils.Constants
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.core.Scheduler
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.observers.DisposableSingleObserver
 import io.reactivex.rxjava3.schedulers.Schedulers
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.launch
-import retrofit2.HttpException
-import retrofit2.Response
 
 class RandomDishViewModel : ViewModel() {
 
